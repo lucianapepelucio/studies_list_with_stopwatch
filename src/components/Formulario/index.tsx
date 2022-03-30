119892b12259fe2c07e741a2a05f6fce96766f36
Luciana <luciana.lucio@g.globo>
2022-03-30 09:01:54 -0300

    add and update tasks

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { ITarefa } from '../../types/tarefa';
 import Botao from '../Botao';
 import style from './Formulario.module.scss';
+import { v4 as uuidv4 } from 'uuid';
 
-class Formulario extends React.Component{
+class Formulario extends React.Component<{
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}> {
     state = {
       tarefa: "",
       tempo: "00:00"
@@ -10,7 +14,21 @@ class Formulario extends React.Component{
 
     adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
       evento.preventDefault();
-      //console.log('state: ', this.state);
+      this.props.setTarefas(tarefasAntigas => 
+        [
+          ...tarefasAntigas, 
+          {
+            ...this.state,
+            selecionado: false,
+            completado: false,
+            id: uuidv4()
+          }
+        ]
+      );
+      this.setState({
+        tarefa: "",
+        tempo: "00:00"
+      })
     }
 
     render() {
@@ -46,7 +64,7 @@ class Formulario extends React.Component{
                     required
                   />
                 </div>
-                <Botao>
+                <Botao type="submit">
                   Adicionar
                 </Botao>
             </form>
--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { ITarefa } from '../../../types/tarefa';
 import style from '../Lista.module.scss';
 
-export default function Item({tarefa, tempo}: {tarefa: string, tempo: string}) {
+export default function Item({tarefa, tempo, selecionado, completado, id}: ITarefa) {
+    console.log('Item atual: ', {tarefa, tempo, selecionado, completado, id});
     return(
         <li className={style.item}>
             <h3>{tarefa}</h3>
